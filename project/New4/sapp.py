82682318e58dcf2994aefe179ee1533c75986b7d
suhaschalak <154907835+suhaschalak@users.noreply.github.com>
2025-01-24 00:04:20 +0530

    Update sapp.py

--- project/New4/sapp.py
+++ project/New4/sapp.py
@@ -88,13 +88,13 @@ st.markdown(
 )
 
 # Sidebar with image and description
-st.sidebar.image('project/New4/image.png', caption="Dermatrix",use_column_width=True)
+st.sidebar.image('project/New4/image.png', caption="Dermatrix", use_container_width=True)
 st.sidebar.markdown(
     "<div class='sidebar-text'>Accurate detection of skin diseases with remedies suggestions to help manage them effectively.</div>",
     unsafe_allow_html=True
 )
 
-st.markdown('<div class="title">Skin Disease Detection and with Remedey Suggestion</div>', unsafe_allow_html=True)
+st.markdown('<div class="title">Skin Disease Detection and Remedy Suggestion</div>', unsafe_allow_html=True)
 st.markdown('<div class="subtitle">Upload an image or use your webcam to detect skin diseases and get remedies.</div>', unsafe_allow_html=True)
 
 # Image input
@@ -104,7 +104,7 @@ if upload_option == 'Upload Image':
     uploaded_file = st.file_uploader("Choose an image file", type=['jpg', 'jpeg', 'png'])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
+        st.image(image, caption='Uploaded Image', use_container_width=True)
         predict_button = st.button("Predict Disease")
         if predict_button:
             disease, confidence = predict_disease(image)
@@ -117,7 +117,7 @@ elif upload_option == 'Use Webcam':
     camera_image = st.camera_input("Take a picture")
     if camera_image is not None:
         image = Image.open(camera_image)
-        st.image(image, caption='Captured Image', use_column_width=True)
+        st.image(image, caption='Captured Image', use_container_width=True)
         predict_button = st.button("Predict Disease")
         if predict_button:
             disease, confidence = predict_disease(image)
